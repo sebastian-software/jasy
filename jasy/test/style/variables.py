e1238bbf7ea0971490b01a6eb896a7a7ef49a499
Sebastian Werner <info@sebastian-werner.net>
2013-08-11 09:36:08 +0200

    Added variable test suite

---
+++ jasy/test/style/variables.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys, os, unittest, logging, inspect
+
+# Extend PYTHONPATH with local 'lib' folder
+if __name__ == "__main__":
+    jasyroot = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]), os.pardir, os.pardir, os.pardir, os.pardir))
+    sys.path.insert(0, jasyroot)
+    print("Running from %s..." % jasyroot)
+
+import jasy.style.Engine as Engine
+
+"""
+SUPPORTS
+
+$width = 300px;
+
+"""
+
+class Tests(unittest.TestCase):
+
+    def process(self, code):
+        callerName = inspect.stack()[1][3][5:]
+
+        tree = Engine.getTree(code, callerName)
+        tree = Engine.processTree(tree)
+        return Engine.compressTree(tree)
+
+    def test_reference_before(self):
+        self.assertEqual(self.process('''
+            $width = 300px;
+            '''), 'x')
+        
+
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.ERROR)
+    suite = unittest.TestLoader().loadTestsFromTestCase(Tests)
+    unittest.TextTestRunner(verbosity=2).run(suite)   
+
