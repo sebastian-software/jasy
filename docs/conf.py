75ce8dd096dcae5002c0802f8aab6439c1266c22
Sebastian Werner <info@sebastian-werner.net>
2013-09-09 16:59:57 +0200

    Cleanup for readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,6 @@
 
 import sys, os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, '/Users/Sebastian/Workspace/jasy')
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -92,7 +87,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+#html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
