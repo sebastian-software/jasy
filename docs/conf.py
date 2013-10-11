8c99fd426676c385eae5f9117ddcd318522b00ae
Sebastian Werner <info@sebastian-werner.net>
2013-10-11 14:41:00 +0200

    Minor fix

--- docs/conf.py
+++ docs/conf.py
@@ -107,10 +107,10 @@ pygments_style = 'sphinx'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Jasy 1.5-alpha3'
+html_title = 'Jasy %s' % version
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Jasy 1.5-alpha3'
+html_short_title = 'Jasy %s' % version
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
