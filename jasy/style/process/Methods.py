7dadc56e797fda034b221de2d27e604997052e0d
Sebastian Werner <info@sebastian-werner.net>
2013-12-06 15:11:02 +0100

    Make use of return values from execute command

--- jasy/style/process/Methods.py
+++ jasy/style/process/Methods.py
@@ -95,11 +95,20 @@ def __executeRecurser(node, session):
             params.append(value)
 
         # print("Looking for command: %s(%s)" % (command, ", ".join([str(param) for param in params])))
-        result = session.executeCommand(command, params)
-        # print("Result: %s" % result)
+        result, restype = session.executeCommand(command, params)
 
-        repl = Node.Node(type="identifier")
-        repl.value = result
+        if restype == "px":
+            repl = Node.Node(type="number")
+            repl.value = result
+            repl.unit = restype
+
+        elif restype == "url":
+            repl = Node.Node(type="identifier")
+            repl.value = "url(%s)" % result
+
+        else:
+            repl = Node.Node(type="identifier")
+            repl.value = result
 
         node.parent.replace(node, repl)
 
