aaa664781ac2dabf8fe2e2debcd2373d5c07dfcd
Sebastian Werner <info@sebastian-werner.net>
2013-12-27 11:06:27 +0100

    Basic variable replacement implemented

--- jasy/style/process/Executer.py
+++ jasy/style/process/Executer.py
@@ -28,6 +28,24 @@ def __recurser(node, values):
     print("PROCESS: %s from %s" % (node.type, node.line))
 
 
+
+
+    # Replace variable with actual value
+    if node.type == "variable" and not (node.parent.type == "assign" and node.parent[0] is node):
+        name = node.name
+        if not name in values:
+            raise VariableError("Could not resolve variable %s! Missing value!" % name, node)
+
+        value = values[name]
+        if value is None:
+            raise VariableError("Could not resolve variable %s! Value is none!" % name, node)
+
+        Console.debug("Resolving variable: %s at line %s with %s from %s", name, node.line, values[name].type, values[name].line)
+        node.parent.replace(node, copy.deepcopy(values[name]))
+
+
+
+
     for child in list(node):
         # Ignore non-children... through possible interactive structure changes
         if child and child.parent is node:
@@ -63,3 +81,35 @@ def __recurser(node, values):
 
         # Remove declaration node from tree
         node.parent.remove(node)
+
+
+
+    elif node.type in ("property", "selector") and getattr(node, "dynamic", False):
+        def replacer(matchObj):
+            name = matchObj.group(1)
+
+            if not name in values:
+                raise VariableError("Could not resolve variable %s! Missing value!" % name, node)
+
+            value = values[name]
+            if value is None:
+                raise VariableError("Could not resolve variable %s! Value is none!" % name, node)
+
+            if value.type == "identifier":
+                return value.value
+            elif value.type == "string":
+                return value.value
+            elif value.type == "number":
+                return "%s%s" % (value.value, getattr(value, "unit", ""))
+            else:
+                raise VariableError("Could not replace property inline variable with value of type: %s" % value.type, node)
+
+        # Fix all selectors
+        if node.type == "selector":
+            selectors = node.name
+            for pos, selector in enumerate(selectors):
+                selectors[pos] = RE_INLINE_VARIABLE.sub(replacer, selector)
+
+        else:
+            node.name = RE_INLINE_VARIABLE.sub(replacer, node.name)
+
