1ac199ca6fbecf4ebdb6ea6d30ae1272c7871997
Sebastian Werner <info@sebastian-werner.net>
2014-02-28 14:06:59 +0100

    Added new failing test for @charset

---
+++ jasy/test/style/cssat.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import sys, os, unittest, logging, inspect
+
+# Extend PYTHONPATH with local 'lib' folder
+if __name__ == "__main__":
+    jasyroot = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]), os.pardir, os.pardir, os.pardir, os.pardir))
+    sys.path.insert(0, jasyroot)
+    print("Running from %s..." % jasyroot)
+
+import jasy.style.Engine as Engine
+import jasy.core.Permutation as Permutation
+
+
+
+class Tests(unittest.TestCase):
+
+    def process(self, code):
+        callerName = inspect.stack()[1][3][5:]
+
+        permutation = Permutation.Permutation({
+            "jasy.engine" : "gecko",
+            "jasy.debug" : True
+        })
+
+        tree = Engine.getTree(code, callerName)
+        tree = Engine.permutateTree(tree, permutation)
+        tree = Engine.reduceTree(tree)
+
+        return Engine.compressTree(tree)
+
+
+    def test_charset(self):
+        self.assertEqual(self.process(r'''
+/* Set the encoding of the style sheet to Unicode UTF-8 */
+@charset "UTF-8";
+
+/* Set the encoding of the style sheet to Latin-9 (Western European languages, with euro sign) */
+@charset 'iso-8859-15';
+            '''), '')
+
+
+    def test_charset_wrong(self):
+        self.assertEqual(self.process(r'''
+/* Invalid as it requires quotes */
+@charset iso-8859-15;
+            '''), '')
+
+
+
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.ERROR)
+    suite = unittest.TestLoader().loadTestsFromTestCase(Tests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
