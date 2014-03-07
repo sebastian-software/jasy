fa233c3c0234d6070c3617fc21749761d49fd4a2
Sebastian Werner <info@sebastian-werner.net>
2014-03-07 15:53:20 +0100

    Renamed public reference

--- jasy/env/Context.py
+++ jasy/env/Context.py
@@ -13,7 +13,7 @@ from jasy.env.State import session
 import jasy.core.Console as Console
 import jasy.env.Task as Task
 import jasy.vcs.Repository as Repository
-import jasy.build.Manager as BuildManager
+import jasy.build.Manager as Build
 
 # Common Classes
 from jasy.core.Profile import Profile
