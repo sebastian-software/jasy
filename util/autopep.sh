5b7c3dd1b97eb245323d6ef5635a08ffc1dc2bf6
Sebastian Werner <info@sebastian-werner.net>
2014-04-09 21:30:27 +0200

    More verbose autopep command + added docformatter

--- util/autopep.sh
+++ util/autopep.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # Automatically applies PEP8 compliance for all Python files
-autopep8 -aaa --in-place --ignore E203,E302,E303 --max-line-length 1000 `find jasy -name "*.py"`
+autopep8 --aggressive --aggressive --aggressive --in-place --ignore E203,E302,E303 --max-line-length 1000 `find jasy -name "*.py"`
+
+# Automatically format docs
+docformatter --in-place `find jasy -name "*.py"`
