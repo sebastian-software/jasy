74dfa0c6d09e0c87ddba656d67ed52d04d1393d5
Sebastian Werner <info@sebastian-werner.net>
2014-04-09 21:21:42 +0200

    Corrected comment and improved aggressiveness :)

--- util/autopep.sh
+++ util/autopep.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Checks PEP8 compliance for all Python files (style checks)
-autopep8 -aa --in-place --ignore E303,E203 --max-line-length 1000 `find jasy -name "*.py"`
+# Automatically applies PEP8 compliance for all Python files
+autopep8 -aaa --in-place --ignore E303,E203 --max-line-length 1000 `find jasy -name "*.py"`
