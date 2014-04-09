529ff797033e0f98a2e3667fa57d4f7e86935b8a
Sebastian Werner <info@sebastian-werner.net>
2014-04-09 21:42:48 +0200

    Tweaks for auto docformatting

--- util/autopep.sh
+++ util/autopep.sh
@@ -4,4 +4,4 @@
 autopep8 --aggressive --aggressive --aggressive --in-place --ignore E203,E302,E303 --max-line-length 1000 `find jasy -name "*.py"`
 
 # Automatically format docs
-docformatter --in-place `find jasy -name "*.py"`
+docformatter --in-place --pre-summary-newline --wrap-summaries 120 --wrap-descriptions 120 `find jasy -name "*.py"`
